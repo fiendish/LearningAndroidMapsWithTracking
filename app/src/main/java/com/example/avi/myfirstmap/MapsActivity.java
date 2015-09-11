507e371c2bb35fc865895d771b90bd613c0f44a3
fiendish <patcherton.fixesthings@gmail.com>
2015-09-11 17:27:21 -0400

    add Drive api stuff for cloud storage, safer location access at launch, and a few name tweaks

--- app/src/main/java/com/example/avi/myfirstmap/MapsActivity.java
+++ app/src/main/java/com/example/avi/myfirstmap/MapsActivity.java
@@ -1,5 +1,7 @@
 package com.example.avi.myfirstmap;
 
+import android.content.Intent;
+import android.content.IntentSender;
 import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -7,13 +9,23 @@ import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
+
+
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.DriveId;
+import com.google.android.gms.drive.DriveResource;
+import com.google.android.gms.drive.Metadata;
+import com.google.android.gms.drive.OpenFileActivityBuilder;
+
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
@@ -21,9 +33,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity
         implements
@@ -36,12 +46,11 @@ public class MapsActivity extends FragmentActivity
     private TextView mMessageView;
     private GoogleMap mMap;
     private boolean mTrackPosition = false;
-    private ImageView imgMyLocationTrackingOff;
-
+    private ImageView mImgMyLocationTracking;
+    private DriveId mDriveFolderId = null;
     private static final boolean YES_ZOOM = true;
     private static final boolean NO_ZOOM = false;
-
-    //private FrameLayout imgMyLocationTrackingOffContainer;
+    private Location mLastLocation = null;
 
     // These settings are the same as the settings for the map. They will in fact give you updates
     // at the maximal rates currently possible.
@@ -55,13 +64,13 @@ public class MapsActivity extends FragmentActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        imgMyLocationTrackingOff = (ImageView) findViewById(R.id.imgMyLocationTrackingOff);
-        ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) imgMyLocationTrackingOff.getLayoutParams();
+        mImgMyLocationTracking = (ImageView) findViewById(R.id.imgMyLocationTracking);
+        ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) mImgMyLocationTracking.getLayoutParams();
         int pad = getStatusBarHeight();
-        lp.setMargins(lp.leftMargin, pad*2, pad, lp.bottomMargin);
-        imgMyLocationTrackingOff.setLayoutParams(lp);
+        lp.setMargins(lp.leftMargin, pad * 2, pad, lp.bottomMargin);
+        mImgMyLocationTracking.setLayoutParams(lp);
 
-        imgMyLocationTrackingOff.setOnClickListener(new View.OnClickListener() {
+        mImgMyLocationTracking.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 onMyLocationButtonClick();
             }
@@ -69,6 +78,8 @@ public class MapsActivity extends FragmentActivity
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(LocationServices.API)
+                .addApi(Drive.API)
+                .addScope(Drive.SCOPE_FILE)
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .build();
@@ -111,8 +122,12 @@ public class MapsActivity extends FragmentActivity
     }
 
     private Location getMyLocation() {
-        return LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        //TODO: LOG! Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+        Location new_loc =  LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (new_loc == null) {
+            return mLastLocation;
+        } else {
+            return new_loc;
+        }
     }
 
     private void panToLocation(Location location, boolean zoom_in) {
@@ -151,13 +166,13 @@ public class MapsActivity extends FragmentActivity
      */
     @Override
     public void onLocationChanged(Location location) {
+        mLastLocation = location;
         if (mTrackPosition) {
            panToLocation(location, NO_ZOOM);
         }
 
         Log.i("Location Listener", location.toString());
-        // mMessageView.setText("Location = " + location);
-    }
+     }
 
     /**
      * Callback called when connected to GCore. Implementation of {@link GoogleApiClient.ConnectionCallbacks}.
@@ -168,7 +183,52 @@ public class MapsActivity extends FragmentActivity
                 mGoogleApiClient,
                 REQUEST,
                 this);  // LocationListener
+
+        mImgMyLocationTracking.setVisibility(View.VISIBLE);
+
+        if (mDriveFolderId != null) {
+            DriveFolder folder = Drive.DriveApi.getFolder(mGoogleApiClient, mDriveFolderId);
+            folder.getMetadata(mGoogleApiClient).setResultCallback(metadataRetrievedCallback);
+        } else {
+            getNewFolder();
+        }
     }
+
+    private void getNewFolder() {
+        mDriveFolderId = null;
+        IntentSender intentSender = Drive.DriveApi.newOpenFileActivityBuilder()
+                .setActivityTitle("Pick a destination folder for stored track data")
+                .setMimeType(new String[]{DriveFolder.MIME_TYPE})
+                .build(mGoogleApiClient);
+
+        try {
+            startIntentSenderForResult(intentSender, REQUEST_CODE.FILE_OPENER.ordinal(), null, 0, 0, 0);
+        } catch (IntentSender.SendIntentException e) {
+            Log.w("INTENT ERROR", "Unable to send intent FILE_OPENER", e);
+        }
+    }
+
+    final private ResultCallback<DriveResource.MetadataResult> metadataRetrievedCallback = new
+            ResultCallback<DriveResource.MetadataResult>() {
+                @Override
+                public void onResult(DriveResource.MetadataResult result) {
+                    if (!result.getStatus().isSuccess()) {
+                        Log.i("METADATA", "Problem while trying to fetch metadata.");
+                        getNewFolder();
+                        return;
+                    }
+
+                    Metadata metadata = result.getMetadata();
+                    if(metadata.isTrashed()){
+                        Log.i("METADATA", "Folder is trashed");
+                        getNewFolder();
+                    }else{
+                        Log.i("METADATA", "Folder is not trashed");
+                    }
+
+                }
+            };
+
 
     /**
      * Callback called when disconnected from GCore. Implementation of {@link GoogleApiClient.ConnectionCallbacks}.
@@ -178,26 +238,68 @@ public class MapsActivity extends FragmentActivity
         // Do nothing
     }
 
+    private enum REQUEST_CODE {
+        RESOLVE_CONNECTION,
+        FILE_OPENER
+    }
+
+    private static final REQUEST_CODE[] RC = REQUEST_CODE.values();
+
     /**
      * Implementation of {@link GoogleApiClient.OnConnectionFailedListener}.
      */
     @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        // Do nothing
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                Log.i("CONNECTION RETRY?", "RETRY");
+                connectionResult.startResolutionForResult(this, REQUEST_CODE.RESOLVE_CONNECTION.ordinal());
+            } catch (IntentSender.SendIntentException e) {
+                Log.e("CONNECTION EXCEPTION", "Exception while starting resolution activity", e);
+            }
+        } else {
+            GooglePlayServicesUtil.getErrorDialog(connectionResult.getErrorCode(), this, 0).show();
+        }
     }
 
-    public boolean onMyLocationButtonClick() {
+    @Override
+    protected void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
+        switch (RC[requestCode]) {
+            case RESOLVE_CONNECTION:
+                if (resultCode == RESULT_OK) {
+                    mGoogleApiClient.connect();
+                }
+                break;
+            case FILE_OPENER:
+                if (resultCode == RESULT_OK) {
+                    mDriveFolderId = data.getParcelableExtra(OpenFileActivityBuilder.EXTRA_RESPONSE_DRIVE_ID);
+                    Log.i("OPENER", "Selected folder's ID: " + mDriveFolderId);
+                }
+                break;
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+                break;
+        }
+    }
 
-        mTrackPosition = !mTrackPosition;
+    private boolean onMyLocationButtonClick() {
 
-        //Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
-        Log.i("MyLocation button", "clicked. Follow?"+mTrackPosition);
+        Location my_loc = getMyLocation();
 
-        if (mTrackPosition) {
-            imgMyLocationTrackingOff.setImageResource(R.mipmap.tracking_on);
-            panToLocation(getMyLocation(), YES_ZOOM);
-        } else {
-            imgMyLocationTrackingOff.setImageResource(R.mipmap.tracking_off);
+        if (my_loc != null) {
+            mTrackPosition = !mTrackPosition;
+
+            //Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+            Log.i("MyLocation button", "clicked. Follow?" + mTrackPosition);
+
+            if (mTrackPosition) {
+                mImgMyLocationTracking.setImageResource(R.mipmap.tracking_on);
+                mImgMyLocationTracking.setContentDescription(getString(R.string.disable_panning));
+                panToLocation(my_loc, YES_ZOOM);
+            } else {
+                mImgMyLocationTracking.setImageResource(R.mipmap.tracking_off);
+                mImgMyLocationTracking.setContentDescription(getString(R.string.enable_panning));
+            }
         }
 
         // Return false so that we don't consume the event and the default behavior still occurs
